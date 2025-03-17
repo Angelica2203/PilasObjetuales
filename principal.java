de4e6ddca6d8af4d8f55d5f30ff76c672535fcbd
Angelica2203 <gaonaangelica22@gmail.com>
2025-03-17 06:23:26 -0500

    cambio

--- principal.java
+++ principal.java
@@ -1,15 +1,7 @@
-
 import java.util.Stack;
-
-public class principal 
-{
-    public static void main(String[] args) 
-    {
-        metodos m =  new metodos();
-        Stack<Vehiculo> pila = m.LlenarPila();
-        m.MostrarPila(pila);
-        m.Eliminar(pila);
-        
+public class principal {
+    public static void main(String[] args) {
+        metodos m = new metodos();
+        m.LlenarPila();
     }
-    
 }
